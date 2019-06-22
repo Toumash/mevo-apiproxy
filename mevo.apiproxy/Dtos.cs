f9053d03d71c22af92a132989faacb8edb9c00be
Tomasz Dłuski <mr.toumash@gmail.com>
2019-06-22 15:17:12 +0200

    initial commit

---
+++ mevo.apiproxy/Dtos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mevo.apiproxy
+{
+    public class Place
+    {
+        public int uid { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public bool bike { get; set; }
+        public string name { get; set; }
+        public object address { get; set; }
+        public bool spot { get; set; }
+        public int number { get; set; }
+        public int bikes { get; set; }
+        public int booked_bikes { get; set; }
+        public int bike_racks { get; set; }
+        public int free_racks { get; set; }
+        public int special_racks { get; set; }
+        public int free_special_racks { get; set; }
+        public bool maintenance { get; set; }
+        public string terminal_type { get; set; }
+        public List<BikeList> bike_list { get; set; }
+        public List<string> bike_numbers { get; set; }
+        public object bike_types { get; set; }
+        public string place_type { get; set; }
+        public bool rack_locks { get; set; }
+        public string city { get; set; }
+    }
+
+    public class MevoApiRootDto
+    {
+        public List<Place> places { get; set; }
+    }
+    public class BatteryPack
+    {
+        public int percentage { get; set; }
+    }
+
+    public class BikeList
+    {
+        public string number { get; set; }
+        public int bike_type { get; set; }
+        public List<string> lock_types { get; set; }
+        public bool active { get; set; }
+        public string state { get; set; }
+        public bool electric_lock { get; set; }
+        public long boardcomputer { get; set; }
+        public int pedelec_battery { get; set; }
+        public BatteryPack battery_pack { get; set; }
+    }
+}
---
+++ mevo.apiproxy/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mevo.apiproxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var driver = CreateDriver())
+            {
+                driver.Navigate().GoToUrl("https://rowermevo.pl/mapa-stacji/?city=Gda%C5%84sk&place=#city-switcher");
+                var bike_data = (string)driver.ExecuteScript("return NEXTBIKE_PLACES_DB;");
+
+                // following line removes [ and ] on the both ends of the variable in javascript.
+                // Without the string cut the string is not a valid json object
+                bike_data = bike_data.Substring(1, bike_data.Length - 2);
+                var data = JsonConvert.DeserializeObject<MevoApiRootDto>(bike_data);
+
+                var gdansk = data.places.Where(p => p.city == "Gdańsk").ToList();
+                var notEmptyPlaces = gdansk.Where(p => p.bike_numbers.Any()).ToList();
+                var places = notEmptyPlaces.Select(p => new { p.bike_numbers, p.bike, p.lat, p.lng });
+                Console.WriteLine(JsonConvert.SerializeObject(places));
+            }
+        }
+
+        private static ChromeDriver CreateDriver()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions
+            {
+                LeaveBrowserRunning = false,
+                UnhandledPromptBehavior = UnhandledPromptBehavior.Ignore
+            };
+            chromeOptions.AddArgument("headless");
+            return new ChromeDriver(".", chromeOptions);
+        }
+    }
+}
